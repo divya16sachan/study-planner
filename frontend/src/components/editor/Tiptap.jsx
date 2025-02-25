c8d9f467647229d6ced71f3c51be230924fa0667
Abhijeet Singh Rajput <abhijeet62008@gmail.com>
2025-02-26 00:32:04 +0530

    feat: enhance note loading mechanism with localStorage caching.

--- frontend/src/components/editor/Tiptap.jsx
+++ frontend/src/components/editor/Tiptap.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { extensions } from './config/extensions.config'
-import { EditorProvider } from '@tiptap/react'
-import { useParams } from 'react-router-dom'
-import { useNoteStore } from '@/stores/useNoteStore'
-import NoteSkeleton from '../sekeletons/NoteSkeleton'
+import React, { useEffect, useState } from 'react';
+import { extensions } from './config/extensions.config';
+import { EditorProvider } from '@tiptap/react';
+import { useParams } from 'react-router-dom';
+import { useNoteStore } from '@/stores/useNoteStore';
+import NoteSkeleton from '../sekeletons/NoteSkeleton';
 import { MenuBar } from './MenuBar';
 
 const Tiptap = () => {
@@ -16,35 +16,55 @@ const Tiptap = () => {
     useEffect(() => {
         const fetchData = async () => {
             if (noteId) {
-                const noteContent = await getNoteContent(noteId);
-                if(noteContent === null){
-                    setNotFound(true);
+                const storedData = JSON.parse(localStorage.getItem('noteContent'));
+                if (storedData && storedData.noteId === noteId) {
+                    setContent(storedData.content);
+                } else {
+                    const noteContent = await getNoteContent(noteId);
+                    if (noteContent === null || noteContent === '') {
+                        setNotFound(true);
+                    } else {
+                        setContent(noteContent);
+                    }
                 }
-                setContent(noteContent);
                 setLoading(false);
             }
         };
         fetchData();
     }, [noteId, getNoteContent]);
 
+    useEffect(() => {
+        if (content) {
+            const data = { noteId, content };
+            localStorage.setItem('noteContent', JSON.stringify(data));
+        }
+    }, [content, noteId]);
+
+    const handleUpdate = (newContent) => {
+        setContent(newContent);
+    };
+
     if (notFound) {
         return (
             <div className='w-full h-full flex items-center justify-center'>
                 <img src='/404-not-found.svg' className='p-4 rounded-lg max-w-[500px]'></img>
             </div>
-        )
+        );
     }
 
     if (isContentLoading || loading) {
         return <NoteSkeleton />;
     }
+
     return (
         <EditorProvider
             className="h-full opacity-0"
             slotBefore={<MenuBar noteId={noteId} />}
             extensions={extensions}
             content={content}
+            onUpdate={({ editor }) => handleUpdate(editor.getHTML())}
         />
     );
 };
+
 export default Tiptap;
