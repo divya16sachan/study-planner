811b320bd4e867ab7b035ab2e21ae468da1b1e87
Divya Sachan <divya16sachan@gmail.com>
2025-05-16 08:39:09 +0530

    Implement user authentication with signup and login functionality

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './db.js';
 
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
 const app = express();
@@ -14,6 +16,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello from server" });
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
