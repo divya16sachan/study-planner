6b673362bd0da537fe6528b6017e278eda3344cb
Divya Sachan <divya16sachan@gmail.com>
2025-05-23 02:54:41 +0530

    feat: add FlipTimer component for countdown display using flip animation

---
+++ frontend/src/components/FlipTimer.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useRef } from "react";
+import Tick from "@pqina/flip";
+
+const FlipTimer = () => {
+  const tickRef = useRef(null);
+
+  useEffect(() => {
+    if (tickRef.current) {
+      Tick.DOM.create(tickRef.current, {
+        didInit: handleTickInit,
+      });
+    }
+  }, []);
+
+  function handleTickInit(tick) {
+    const locale = {
+      HOUR_PLURAL: "Hours",
+      HOUR_SINGULAR: "Hour",
+      MINUTE_PLURAL: "Minutes",
+      MINUTE_SINGULAR: "Minute",
+      SECOND_PLURAL: "Seconds",
+      SECOND_SINGULAR: "Second",
+    };
+
+    Object.keys(locale).forEach((key) => tick.setConstant(key, locale[key]));
+    const now = new Date();
+    const todayMidnight = `${now.getFullYear()}-${(now.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")}T00:00:00`;
+
+    Tick.count.up(todayMidnight, {
+      format: ["h", "m", "s"],
+    }).onupdate = (value) => {
+      tick.value = value;
+    };
+  }
+
+  return (
+    <div ref={tickRef} className="tick">
+      <div className="flex gap-1" data-repeat="true" data-layout="horizontal fit" data-transform="preset(h, m, s) -> delay">
+        <div className="tick-group">
+          <div data-key="value" data-repeat="true" data-transform="pad(00) -> split -> delay">
+            <span data-view="flip"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlipTimer;
